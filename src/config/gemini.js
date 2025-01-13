12fa27dd441173aae8c1a278cbec0ebd57a555f2
Madhura Shirsikar <121761774+MadhuraS422@users.noreply.github.com>
2025-01-13 16:02:29 +0530

    Add files via upload

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -1,1 +1,38 @@
+/* eslint-disable no-unused-vars */
 
+import {
+    GoogleGenerativeAI,
+    HarmCategory,
+    HarmBlockThreshold,
+  } from "@google/generative-ai"
+  
+  const apiKey = 'AIzaSyAMk1yudKnhjgsLAtW7gKdqKaxjJJbXC9Y';
+  const genAI = new GoogleGenerativeAI(apiKey);
+  
+  const model = genAI.getGenerativeModel({
+    model: "gemini-1.5-flash",
+  });
+  
+  const generationConfig = {
+    temperature: 1,
+    topP: 0.95,
+    topK: 64,
+    maxOutputTokens: 8192,
+    responseMimeType: "text/plain",
+  };
+  
+  async function run(prompt) {
+    const chatSession = model.startChat({
+      generationConfig,
+      history: [
+      ],
+    });
+  
+    const result = await chatSession.sendMessage(prompt);
+    const response = result.response;
+    console.log(result.response.text());
+    return response.text();
+  }
+  
+  // eslint-disable-next-line no-undef
+  export default run;
